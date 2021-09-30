b16099adf3c4764c171b35e3ffbbe3b134eb28a2
SanjanaaZola <zolasanjanaa@gail.com>
2021-09-30 19:40:36 +0530

    updated utils

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState(new Values('#800080').all(7.5))
+  const [list, setList] = useState(new Values('#F15025').all(7.5))
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -27,7 +27,7 @@ function App() {
             type='text'
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder='#800080'
+            placeholder='#F15025'
             className={`${error ? 'error' : null}`}
           />
           <button className='btn' type='submit'>
